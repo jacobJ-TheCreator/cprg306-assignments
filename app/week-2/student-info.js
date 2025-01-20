8649dd253cf28e33004ca0c7c6ee4036337760bb
Jacob Jobse <jakejobse1@gmail.com>
2025-01-20 15:27:04 -0700

    Assignment 2

---
+++ app/week-2/student-info.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+export default function StudentInfo (){
+    return (
+        <div>
+            <p>Jacob Jobse</p>
+            <p><Link href = "https://github.com/jacobJ-TheCreator/cprg306-assignments">https://github.com/jacobJ-TheCreator/cprg306-assignments</Link></p>
+            
+        </div>
+    );
+}
