6465bf8762f20f32288bf2845616e67a05e5361a
Jacob Jobse <jakejobse1@gmail.com>
2025-02-03 13:53:49 -0700

    week-3 assignment
    Complete

--- app/page.js
+++ app/page.js
@@ -8,7 +8,8 @@ export default function Home (){
       <header>
         CPRG 306: Web Development 2 - Assignments
       </header>
-      <p><Link href = "http://localhost:3001/week-2">Week 2 Assignment</Link></p>
+      <p><Link href = "http://localhost:3000/week-2">Week 2 Assignment</Link></p>
+      <p><Link href = "http://localhost:3000/week-3">Week 3 Assignment</Link></p>
     </div>
   );
 
---
+++ app/week-3/page.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ItemList from "./item-list"; // Ensure the correct case-sensitive file name
+
+export default function Page() {
+  return (
+    <main className="min-h-screen bg-purple-200 p-6">
+        
+      <h1 className="text-3xl font-bold text-gray-700 mb-6">Shopping List</h1>
+      <ItemList />
+    </main>
+  );
+}
+
+
